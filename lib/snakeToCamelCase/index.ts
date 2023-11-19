ccdf7a578af7255ebf2d01329031bfecdeedb7c5
IMSHoJung <IMSHoJung@imsmobility.co.kr>
2023-11-19 18:28:30 +0900

    fix: prettier 수정, array요소가 스트링일때변환 되는현상 수정

--- lib/snakeToCamelCase/index.ts
+++ lib/snakeToCamelCase/index.ts
@@ -4,19 +4,16 @@ export const singleConvertSnakeToCamel = (str: string) => {
   return str.replace(/_+([a-z])/g, (_, letter) => letter.toUpperCase());
 };
 
-const snakeToCamelCase = <T,>(arg: T): SnakeToCamelType<T> => {
+const snakeToCamelCase = <T>(arg: T): SnakeToCamelType<T> => {
   if (Array.isArray(arg)) {
     return arg.map((item) => snakeToCamelCase(item)) as any;
   }
+
   if (arg !== null && typeof arg === 'object') {
     return Object.entries(arg).reduce((acc, [key, value]) => {
       const camelKey = singleConvertSnakeToCamel(key) as keyof T;
       return { ...acc, [camelKey]: snakeToCamelCase(value) } as any;
     }, {}) as any;
-  }
-
-  if (typeof arg === 'string') {
-    return singleConvertSnakeToCamel(arg) as any;
   }
 
   return arg as any;
